8507b0000602b6690bd1836419e62a59d171666c
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2023-12-08 02:49:44 +0530

    enchilada: Define TARGET_BOOT_ANIMATION_RES := 1080

--- awaken_enchilada.mk
+++ awaken_enchilada.mk
@@ -23,6 +23,7 @@ $(call inherit-product, device/oneplus/enchilada/device.mk)
 
 # Inherit some common Awaken stuff.
 $(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := awaken_enchilada
 PRODUCT_DEVICE := enchilada
